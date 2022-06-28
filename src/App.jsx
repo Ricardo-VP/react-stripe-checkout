f441a279cfed406aff71b710725c6c343e800803
ricardo-vp <ricardo.vacape@gmail.com>
2022-06-27 22:18:22 -0500

    Adding checkout form 🥵

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import "./App.css";
 import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_KEY);
 
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button>Buy</button>
+    </form>
+  );
+};
+
 function App() {
   console.log(import.meta.env.VITE_STRIPE_KEY);
-  return <h1>Hello World</h1>;
+  return (
+    <Elements stripe={stripePromise}>
+      <CheckoutForm />
+    </Elements>
+  );
 }
 
 export default App;
